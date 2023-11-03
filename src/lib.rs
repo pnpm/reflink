295cdfff811766ca42950e2ac2e05a1e58cd3cc0
Ignacio Aldama Vicente <sr.drabx@gmail.com>
2023-11-03 09:03:12 +0100

    types: fix types issues

--- src/lib.rs
+++ src/lib.rs
@@ -4,11 +4,10 @@
 extern crate napi_derive;
 use copy_on_write::reflink_file_sync;
 use napi::{bindgen_prelude::AsyncTask, Env, Error, JsNumber, Result, Task};
-use std::path::PathBuf;
 
 pub struct AsyncReflink {
-    src: PathBuf,
-    dst: PathBuf,
+    src: String,
+    dst: String,
 }
 
 #[napi]
@@ -17,23 +16,15 @@ impl Task for AsyncReflink {
     type JsValue = JsNumber;
 
     fn compute(&mut self) -> Result<Self::Output> {
-        let src_str = self.src.to_str().ok_or_else(|| {
-          Error::from_reason("Invalid UTF-8 sequence in source path".to_string())
-        })?;
-
-        let dst_str = self.dst.to_str().ok_or_else(|| {
-          Error::from_reason("Invalid UTF-8 sequence in destination path".to_string())
-        })?;
-
-        match reflink_file_sync(src_str, dst_str) {
+        match reflink_file_sync(&self.src, &self.dst) {
             Ok(_) => {
                 Ok(())
             },
             Err(err) => return Err(Error::from_reason(format!(
                 "{}, reflink '{}' -> '{}'",
                 err.to_string(),
-                self.src.display(),
-                self.dst.display()
+                self.src,
+                self.dst
             ))),
         }
     }
@@ -46,15 +37,13 @@ impl Task for AsyncReflink {
 // Async version
 #[napi(js_name = "reflinkFile")]
 pub fn reflink_task(src: String, dst: String) -> AsyncTask<AsyncReflink> {
-    let src_path = PathBuf::from(src);
-    let dst_path = PathBuf::from(dst);
-    AsyncTask::new(AsyncReflink { src: src_path, dst: dst_path })
+    AsyncTask::new(AsyncReflink { src, dst })
 }
 
 // Sync version
 #[napi(js_name = "reflinkFileSync")]
 pub fn reflink_sync(env: Env, src: String, dst: String) -> Result<JsNumber> {
-    match reflink_file_sync(src, dst) {
+    match reflink_file_sync(&src, &dst) {
         Ok(_) => Ok(env.create_int32(0)?),
         Err(err) => Err(Error::from_reason(format!(
             "{}, reflink '{}' -> '{}'",
